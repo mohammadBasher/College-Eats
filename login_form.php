07f1a87653c07858345d1eb04c02cdad399370fb
Basher <mohammadbasher098@gmail.com>
2020-05-15 14:53:26 +0530

    Modifying Menu and display features

--- login_form.php
+++ login_form.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 <title>Feedback form</title>
-<link rel="stylesheet" type="text/css" href="registyle.css">
-</head>
-<body>
-	<div class="form" style="height:490px ">
-	<img class="image" src="./images/logo.png" alt=""><br><br>
+<link rel="stylesheet" type="text/css" href="./registyle.css">
+<head>
+<body style="background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('./images/fruit.jpg');">
+	<div class="form" style="background: brown;height:490px;">
+			<img class="image" src="./images/logo.png" alt=""><br><br>
 <form action="login.php" method="POST" autocomplete="off">
 <input class="input" type="text" name="name" placeholder="Name" required><br><br>
 <input class="input" type="password" name="password" placeholder="please enter your password" required><br><br>
@@ -14,8 +14,8 @@
 <input class="input" type="text" name="item" placeholder="Item" required><br><br>
 <textarea class="input" type="text" rows="5" cols="40" name="feedback" placeholder="enter your feedback here" required></textarea><br><br>
   <input  class="input" style="border-radius:2px;background:red;color:white" type="submit"  name="submit" value="Submit">  <br>
-  <button class="input" style="background:green"><a style="color:white;text-decoration:none" href="registration_form.php">Register Here</a></button>
+  <button class="input" style="background:blue"><a style="color:white;text-decoration:none" href="registration_form.php">Register Here</a></button>
 </form>
 </div>
 </body>
-</html>
+</head>
--- registration_form.php
+++ registration_form.php
@@ -2,10 +2,10 @@
 <html>
 <head>
 <title>Registration page</title>
-<link rel="stylesheet" type="text/css" href="registyle.css">
-<head>
-<body>
-	<div class="form">
+<link rel="stylesheet" type="text/css" href="./registyle.css">
+<head >
+<body style="background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('./images/fruit.jpg');">
+	<div class="form" style="background: brown;">
 	<img class="image" src="./images/logo.png" alt=""><br><br>
 <form action="regis.php" method="POST" autocomplete="off">
 <input class="input" type="text" name="name" placeholder="Name" required><br><br>
@@ -13,7 +13,7 @@
 <input class="input"  type="text" name="email" placeholder="Email" required><br><br>
 <input class="input" type="password" name="password" placeholder="please enter your password" required><br><br>
 <input class="input"  type="password" name="c_password" placeholder="please conform your password" required><br><br>
-  <input class="input" style="border-radius:2px;background:red;color:white" type="submit"  name="submit" value="Submit">
+  <input class="input" style="border-radius:2px;background:red;color:white; border-radius: 30px;" type="submit"  name="submit" value="Submit">
 </form>
 <div>
 </body>
